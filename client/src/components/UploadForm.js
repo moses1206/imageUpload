d2167a7625d239effc46875e9647025717d25a19
unknown <mose23@naver.comm>
2022-03-21 15:19:56 +0900

    Image Array Upload

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -10,26 +10,40 @@ import './UploadForm.css'
 export default function UploadForm() {
   const { images, setImages, myImages, setMyImages } = useContext(ImageContext)
 
-  const defaultFileName = '이미지 파일을 업로드 해주세요'
   const [files, setFiles] = useState(null)
-  const [imgSrc, setImgSrc] = useState(null)
-  const [fileName, setFileName] = useState(defaultFileName)
+
+  const [previews, setPreviews] = useState([])
+
   const [percent, setPercent] = useState(0)
   const [isPublic, setIsPublic] = useState(true)
 
-  const imageSelectHandler = (e) => {
+  const imageSelectHandler = async (e) => {
     const imageFiles = e.target.files
     setFiles(imageFiles)
 
-    const imageFile = imageFiles[0]
-    setFileName(imageFile.name)
+    // 배열 이미지 url을 imagePreviews에 저장하기
+    const imagePreviews = await Promise.all(
+      // imageFiles는 배열이 아니라서 ...imageFile로 배열로 변환
+      [...imageFiles].map(async (imageFile) => {
+        return new Promise((resolve, reject) => {
+          // 성공한 결과를 resolve에 실패한 결과를 reject에 입력
+          // resolve에는 이미지의 url 값이 들어감
+          try {
+            const fileReader = new FileReader()
+            fileReader.readAsDataURL(imageFile)
+            fileReader.onload = (e) =>
+              // onload를 통해 url로 이미지를 전달하도록 변환됨
+              resolve({ imgSrc: e.target.result, fileName: imageFile.name })
+          } catch (err) {
+            reject(err)
+          }
+        })
+      })
+    )
 
-    const fileReader = new FileReader()
-    fileReader.readAsDataURL(imageFile)
-    fileReader.onload = (e) => setImgSrc(e.target.result)
+    console.log(imagePreviews)
+    setPreviews(imagePreviews)
   }
-  console.log(files)
-  console.log(imgSrc)
 
   const onSubmit = async (e) => {
     e.preventDefault()
@@ -54,32 +68,44 @@ export default function UploadForm() {
         toast.success('이미지 업로드 성공!!')
       setTimeout(() => {
         setPercent(0)
-        setFileName(defaultFileName)
-        setImgSrc(null)
+        setPreviews([])
       }, 1000)
     } catch (err) {
       toast.error(err.response.data.message)
       setPercent(0)
-      setFileName(defaultFileName)
-      setImgSrc(null)
+      setPreviews([])
       console.log(err)
     }
   }
 
+  const previewImages = previews.map((preview, index) => (
+    <img
+      key={index}
+      style={{ width: 200, height: 200, objectFit: 'cover' }}
+      src={preview.imgSrc}
+      alt=''
+      className={`image-preview ${preview.imgSrc && 'image-preview-show'} `}
+    />
+  ))
+
+  const fileName =
+    previews.length === 0
+      ? '이미지를 업로드 해주세요!!'
+      : previews.reduce(
+          (previews, current) => previews + `${current.fileName},`,
+          ''
+        )
+
   return (
     <form onSubmit={onSubmit}>
-      <img
-        alt=''
-        src={imgSrc}
-        className={`image-preview ${imgSrc && 'image-preview-show'}`}
-      />
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>{previewImages}</div>
       <ProgressBar percent={percent} />
       <div className='file-dropper'>
         {fileName}
         <input
           id='image'
           type='file'
-          multiple='true'
+          multiple={true}
           accept='image/*'
           onChange={imageSelectHandler}
         />
