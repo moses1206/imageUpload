158899b15ab6120ed1a903cbed886019b838e42f
unknown <mose23@naver.comm>
2022-03-22 14:30:08 +0900

    Infinite Scroll

--- client/src/components/ImageList.js
+++ client/src/components/ImageList.js
@@ -1,18 +1,59 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
 import { AuthContext } from '../context/AuthContext'
 import { ImageContext } from '../context/ImageContext'
 import './ImageList.css'
 
 export default function ImageList() {
-  const { images, myImages, isPublic, setIsPublic } = useContext(ImageContext)
+  const {
+    images,
+    myImages,
+    isPublic,
+    setIsPublic,
+    loadMoreImages,
+    imageLoading,
+    imageError,
+  } = useContext(ImageContext)
   const [me] = useContext(AuthContext)
 
-  const imgList = (isPublic ? images : myImages).map((image) => (
-    <Link to={`/images/${image._id}`} key={image.key}>
-      <img alt='' src={`http://localhost:5000/uploads/${image.key}`} />
-    </Link>
-  ))
+  const elementRef = useRef(null)
+
+  useEffect(() => {
+    if (!elementRef.current) return
+    // 움직임을 체킹하기로 한 태그가 눈에 들어올때
+    // entry.isInterseting이 true로 변함
+
+    const observer = new IntersectionObserver(([entry]) => {
+      console.log('intersection', entry.isIntersecting)
+      if (entry.isIntersecting) loadMoreImages()
+    })
+    observer.observe(elementRef.current)
+    // 옵저버로 체킹후 데이터를 불러오고 나서 다시 위로 올리다 보면
+    // 더블로 체킹되어 same Key 오류가 나게 된다. 1번 감지한 후
+    // 옵저버를 풀어주어야 한다.
+    return () => observer.disconnect()
+  }, [loadMoreImages])
+
+  const imgList = isPublic
+    ? images.map((image, index) => (
+        <Link
+          to={`/images/${image._id}`}
+          key={image.key}
+          ref={index + 1 === images.length ? elementRef : null}
+        >
+          <img alt='' src={`http://localhost:5000/uploads/${image.key}`} />
+        </Link>
+      ))
+    : myImages.map((image, index) => (
+        <Link
+          to={`/images/${image._id}`}
+          key={image.key}
+          // 끝에서 3번째 이미지가 나올때 불러오도록 설정
+          ref={index + 3 === myImages.length ? elementRef : null}
+        >
+          <img alt='' src={`http://localhost:5000/uploads/${image.key}`} />
+        </Link>
+      ))
 
   return (
     <div>
@@ -26,6 +67,8 @@ export default function ImageList() {
       )}
 
       <div className='image-list-container'>{imgList}</div>
+      {imageError && <div>Image Loading Error ...</div>}
+      {imageLoading && <div>Image Loading ...</div>}
     </div>
   )
 }
--- client/src/context/ImageContext.js
+++ client/src/context/ImageContext.js
@@ -1,4 +1,10 @@
-import React, { createContext, useState, useEffect, useContext } from 'react'
+import React, {
+  createContext,
+  useState,
+  useEffect,
+  useContext,
+  useCallback,
+} from 'react'
 import axios from 'axios'
 import { AuthContext } from './AuthContext'
 
@@ -8,15 +14,23 @@ export const ImageProvider = (prop) => {
   const [images, setImages] = useState([])
   const [myImages, setMyImages] = useState([])
   const [isPublic, setIsPublic] = useState(false)
+  const [imageUrl, setImageUrl] = useState('/images')
+  const [imageLoading, setImageLoading] = useState(false)
+  const [imageError, setimageError] = useState(false)
 
   const [me] = useContext(AuthContext)
 
   useEffect(() => {
+    setImageLoading(true)
     axios
-      .get('/images')
-      .then((result) => setImages(result.data))
-      .catch((err) => console.error(err))
-  }, [])
+      .get(imageUrl)
+      .then((result) => setImages((prevData) => [...prevData, ...result.data]))
+      .catch((err) => {
+        console.error(err)
+        setimageError(true)
+      })
+      .finally(() => setImageLoading(false))
+  }, [imageUrl])
 
   useEffect(() => {
     if (me) {
@@ -34,6 +48,17 @@ export const ImageProvider = (prop) => {
     }
   }, [me])
 
+  // 처음에는 라스트 아이디가 없으므로 null값 처리를 해준다.
+  const lastImageId = images.length > 0 ? images[images.length - 1]._id : null
+
+  // useCallback은 계속 리랜더링 되는 컴포넌트를 특정조건에만
+  // 리랜더링 되도록 조건을 주어 지속적인 리랜더링을 막아서
+  //  퍼포먼스를 개선한다.
+  const loadMoreImages = useCallback(() => {
+    if (imageLoading || !lastImageId) return
+    setImageUrl(`/images?lastid=${lastImageId}`)
+  }, [lastImageId, imageLoading])
+
   return (
     <ImageContext.Provider
       value={{
@@ -43,6 +68,9 @@ export const ImageProvider = (prop) => {
         setMyImages,
         isPublic,
         setIsPublic,
+        loadMoreImages,
+        imageLoading,
+        imageError,
       }}
     >
       {prop.children}
